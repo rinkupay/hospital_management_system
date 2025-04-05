b0bfa1627b045da375a4c5b027bb0f75a990dcd0
Rinku <rinkumurmupay@gmail.com>
2025-04-05 22:21:08 +0530

    updated backend url

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -19,7 +19,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('http://localhost:5000/api/login', {
+      const response = await fetch('https://hospital-management-system-h1lt.onrender.com/api/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -38,7 +38,7 @@ const SignUp = () => {
     e.preventDefault();
     if (validateForm()) {
       try {
-        const response = await fetch('http://localhost:5000/api/signup', {
+        const response = await fetch('https://hospital-management-system-h1lt.onrender.com/api/signup', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
